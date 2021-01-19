a929d4347107f18ba26343bf8cdc9fa74fb4b573
Ed55555 <ed.vaneersel@gustomsc.com>
2021-01-19 10:37:43 +0100

    Authorize

--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "aws_region" {
   default = "eu-north-1"
 }
 
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = "AKIAI6QUFJ36TYT4HFDA"
+  secret_key = "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"
+}
+
 variable "db_table_name" {
   type    = string
   default = "terraform-learn"
